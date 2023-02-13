01b5d665916b27ff0c5fb56a7bf64cc1e7ce31bf
smbek <s.mbekelu9@gmail.com>
2023-02-13 13:51:41 -0500

    Third Commit. Made new additions to the email and time logging features. Added TimeoutException handling for all the needed elements. Added double-clicking on the "Preview Order" button element due to the problems it has caused throughout testing.
    May need more exception handling in general will need to be added if the code still produces errors at a more than 5% rate. No extensive testing has been done.

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 # imported libraries
+
+
 import User_Key
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common import actions
+from selenium.webdriver import ActionChains
 import undetected_chromedriver as uc
 import time
 from datetime import datetime
@@ -16,7 +23,7 @@ opts = uc.ChromeOptions()
 # Headless option for running without UI
 # opts.add_argument('--headless')
 driver = uc.Chrome(version_main=109)
-driver.implicitly_wait(20)  # gives an implicit wait for 120 seconds for finding all elements
+action = ActionChains(driver)
 
 # Initializing Global Variables
 error_msg = 'No Errors'  # JUST A PLACE HOLDER CURRENTLY
@@ -24,63 +31,117 @@ account_balance = '0.00'
 starting_balance = '0.00'
 time_purchased = []
 ERR = 0
+i = 0
 
 def fidelity_login():
+    global error_msg
+    # Initially getting this url to pick up cookies
+    driver.get('https://www.fidelity.com/')
+    time.sleep(2)  # WAIT ON HOME PAGE
     # Go to login page
     driver.get('https://digital.fidelity.com/prgw/digital/login/full-page')
-    # Enter Username
-    username_input_element = driver.find_element(by=By.ID, value="userId-input")
-    username_input_element.click()
-    username_input_element.send_keys(User_Key.User)
+    try:
+        WebDriverWait(driver, timeout=30).until(EC.element_to_be_clickable((By.ID, 'userId-input')))
+        # Enter Username
+        username_input_element = driver.find_element(by=By.ID, value="userId-input")
+        username_input_element.click()
+        username_input_element.send_keys(User_Key.User)
+    except TimeoutException:
+        error_msg = 'Could Not Log In'
+        driver.close()
+        return
     # Enter Password
     password_input_element = driver.find_element(by=By.ID, value="password")
     password_input_element.click()
     password_input_element.send_keys(User_Key.Password)
     # Send Enter After Password
     password_input_element.send_keys(Keys.ENTER)
-    time.sleep(3)
+    time.sleep(5)  # Wait for login to happen
 
 
 def fidelity_security_purchasing():
     # Global variables
-    global account_balance
+    global account_balance, i
     global starting_balance
     global time_purchased
     global ERR
     global error_msg
-    
-    # Initially getting this url to pick up cookies
-    driver.get('https://www.fidelity.com/')
 
     # Go to 'trade' page
     driver.get('https://digital.fidelity.com/ftgw/digital/trade-equity/index/orderEntry')
 
     for i in range(0, len(User_Key.ETFs)):
         # Default Tade Option is 'Stocks/ETFs' and the whole point of this program is to buy ETFs so no need to change this
+
         # Selecting an Account
-        account_selection = driver.find_element(by=By.XPATH, value='//*[@id="dest-acct-dropdown"]').click()
-        # Using a profile with only 1 account opened
-        driver.find_element(by=By.ID, value='ett-acct-sel-list').click()
+        try:
+            WebDriverWait(driver, timeout=30).until(EC.presence_of_element_located((By.XPATH, '//*[@id="dest-acct-dropdown"]')))
+            driver.find_element(by=By.XPATH, value='//*[@id="dest-acct-dropdown"]').click()
+            # Using a profile with only 1 account opened
+            driver.find_element(by=By.ID, value='ett-acct-sel-list').click()
+        except TimeoutException:
+            error_msg = 'Could Not Find An Account'
+            ERR = 1
+            driver.close()
+            return i
 
         # Finding Account Balance
         # Giving to collect the real time data once and subtract from
         if i == 0:
-            account_balance = driver.find_element(by=By.XPATH, value='//*[@id="eq-ticket__account-balance"]/div/div[2]/span').text
-            account_balance = float(account_balance[1:])  # Omitting the $ in the front of the text to convert string to a float
-            starting_balance = account_balance
-            print(account_balance)
+            try:
+                WebDriverWait(driver, timeout=30).until(EC.presence_of_element_located((By.XPATH, '//*[@id="eq-ticket__account-balance"]/div/div[2]/span')))
+                account_balance = driver.find_element(by=By.XPATH, value='//*[@id="eq-ticket__account-balance"]/div/div[2]/span').text
+                account_balance = float(account_balance[1:])  # Omitting the $ in the front of the text to convert string to a float
+                starting_balance = account_balance
+                print(account_balance)
+            except TimeoutException:
+                error_msg = 'Could Not Find an Account Balance'
+                ERR = 1
+                driver.close()
+                return i
 
         # Selecting the ETF to be purchased (WRITE AS A RECURSIVE FUNCTION BASED OF DATA IN USER_KEY.py)
-        etf_selector_selector = driver.find_element(by=By.ID, value='eq-ticket-dest-symbol')
-        etf_selector_selector.click()
-        etf_selector_selector.send_keys(User_Key.ETFs[i])
-        etf_selector_selector.send_keys(Keys.ENTER)
+        try:
+            WebDriverWait(driver, timeout=30).until(EC.element_to_be_clickable((By.ID, 'eq-ticket-dest-symbol')))
+            etf_selector_selector = driver.find_element(by=By.ID, value='eq-ticket-dest-symbol')
+            etf_selector_selector.click()
+            etf_selector_selector.send_keys(User_Key.ETFs[i])
+            etf_selector_selector.send_keys(Keys.ENTER)
+        except TimeoutException:
+            error_msg = 'Could Not Select an ETF to buy Button'
+            ERR = 1
+            driver.close()
+            return i
+
         # Buy button
-        buy_button = driver.find_element(by=By.XPATH, value='//*[@id="action-buy"]/s-root/div/label').click()
+        try:
+            WebDriverWait(driver, timeout=30).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="action-buy"]/s-root/div/label')))
+            driver.find_element(by=By.XPATH, value='//*[@id="action-buy"]/s-root/div/label').click()
+        except TimeoutException:
+            error_msg = 'Could Not Find Buy Button'
+            ERR = 1
+            driver.close()
+            return i
+
         # Dollars button
-        dollars_button = driver.find_element(by=By.XPATH, value='//*[@id="quantity-type-dollars"]/s-root/div/label').click()
+        try:
+            WebDriverWait(driver, timeout=30).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="quantity-type-dollars"]/s-root/div/label')))
+            driver.find_element(by=By.XPATH, value='//*[@id="quantity-type-dollars"]/s-root/div/label').click()
+        except TimeoutException:
+            error_msg = 'Could Not Find Dollars Button'
+            ERR = 1
+            driver.close()
+            return i
+
         # Market button
-        driver.find_element(by=By.XPATH, value='//*[@id="market-yes"]/s-root/div/label').click()
+        try:
+            WebDriverWait(driver, timeout=30).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="market-yes"]/s-root/div/label')))
+            driver.find_element(by=By.XPATH, value='//*[@id="market-yes"]/s-root/div/label').click()
+        except TimeoutException:
+            error_msg = 'Could Not Find Dollars Button'
+            ERR = 1
+            driver.close()
+            return i
 
         # If statement to ensure that there is enough cash to make the next investment. If not, break from for loop
         next_investment = float(User_Key.ETF_Investments[i])
@@ -91,26 +152,81 @@ def fidelity_security_purchasing():
             print(account_balance)
 
         # Dollar Amount Input
-        dollar_amount = driver.find_element(by=By.ID, value='eqt-shared-quantity')
-        dollar_amount.send_keys(User_Key.ETF_Investments[i])
-        time.sleep(2)
+        try:
+            WebDriverWait(driver, timeout=30).until(EC.element_to_be_clickable((By.ID, 'eqt-shared-quantity')))
+            dollar_amount = driver.find_element(by=By.ID, value='eqt-shared-quantity')
+            dollar_amount.send_keys(User_Key.ETF_Investments[i])
+        except TimeoutException:
+            error_msg = 'Could Not Enter A Dollar Amount'
+            ERR = 1
+            driver.close()
+            return i
+
         # Preview Order Button
-        driver.find_element(by=By.XPATH, value='//*[@id="previewOrderBtn"]').click()
+        try:
+            WebDriverWait(driver, timeout=30).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="previewOrderBtn"]')))
+            preview_order_button = driver.find_element(by=By.XPATH, value='//*[@id="previewOrderBtn"]')
+            action.double_click(on_element=preview_order_button).perform()
+        except TimeoutException:
+            error_msg = 'Could Not Find Preview Order Button'
+            ERR = 1
+            driver.close()
+            return i
+
         # Place Order Button
-        driver.find_element(by=By.XPATH, value='//*[@id="placeOrderBtn"]').click()
+        try:
+            WebDriverWait(driver, timeout=30).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="placeOrderBtn"]')))
+            driver.find_element(by=By.XPATH, value='//*[@id="placeOrderBtn"]').click()
+        except TimeoutException:
+            WebDriverWait(driver, timeout=30).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="previewOrderBtn"]')))
+            action.double_click(on_element=preview_order_button).perform()
+            WebDriverWait(driver, timeout=30).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="placeOrderBtn"]')))
+            driver.find_element(by=By.XPATH, value='//*[@id="placeOrderBtn"]').click()
+        except:
+            error_msg = 'Could Not Find Place Order Button'
+            ERR = 1
+            driver.close()
+            return i
+
         # Date and Time the "Place Order" Button is pressed
         time_purchased.append(datetime.now().strftime("%m/%d/%Y %H:%M:%S"))
-        time.sleep(2)
 
         # If statement determining whether to continue to the next investment or not
         if i != len(User_Key.ETFs)-1:
             # New Order Button
-            driver.find_element(by=By.XPATH, value='//*[@id="eq-ticket__enter-new-order"]').click()
-            time.sleep(2)
+            try:
+                WebDriverWait(driver, timeout=30).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="eq-ticket__enter-new-order"]')))
+                driver.find_element(by=By.XPATH, value='//*[@id="eq-ticket__enter-new-order"]').click()
+            except TimeoutException:
+                error_msg = 'Could Not Find New Order Button'
+                ERR = 1
+                driver.close()
+                return i
     return i
 
 
-def email():
+def email_purchases_creator(num_purchases):
+    purchases = ""
+    for i in range(0, num_purchases):
+        purchase = "{SEC} for ${SEC_INV} @{TIME}\n".format(SEC=User_Key.ETFs[i], SEC_INV=User_Key.ETF_Investments[i], TIME=time_purchased[i])
+        purchases += purchase
+    purchases += "Total Amount Purchased = ${MONEY_TRADED}".format(MONEY_TRADED=User_Key.total_purchases(User_Key.ETF_Investments))
+    return purchases
+
+
+def email_error_msg_creator():
+    global error_msg
+    global ERR
+    if ERR == 1:
+        error_msg_body = """\n
+                            THERE WAS AN ERROR RUNNING THE PROGRAM
+                            Error Message: {ERR}""".format(ERR=error_msg)
+    else:
+        error_msg_body = """\n\nTHERE WAS NO ERROR"""
+    return error_msg_body
+
+
+def email(purchase_summary):
     
     global time_purchased
     global ERR
@@ -120,29 +236,9 @@ def email():
     email_password = User_Key.BotEmail_Password         # Google App Password
     email_receiver = User_Key.User_Email                # Real Email that will be checked
     subject = 'Bot Purchase Report'                     # Email Subject
-    if ERR == 0:
-        # Email Body
-        body = """  Starting Cash Balance = ${INIT_ACC_BAL} 
-                    Cash Balance Left = ${ACC_BAL}
-                    
-                    Purchases:
-                    {SEC1} for ${SEC1_INV} @ {TIME1}
-                    {SEC2} for ${SEC2_INV} @ {TIME2}
-                    {SEC3} for ${SEC3_INV} @ {TIME3}
-                    {SEC4} for ${SEC4_INV} @ {TIME4}
-                    Total Amount Purchased = ${MONEY_TRADED}
-                    
-                    ERRORS: {ERROR_MESSAGE}
-                    
-                    Next Purchases date ~ {TIME5}
-        """.format(INIT_ACC_BAL=starting_balance, ACC_BAL=account_balance, SEC1=User_Key.ETFs[0], SEC2=User_Key.ETFs[1],
-                   SEC3=User_Key.ETFs[2], SEC4=User_Key.ETFs[3], SEC1_INV=User_Key.ETF_Investments[0],
-                   SEC2_INV=User_Key.ETF_Investments[1], SEC3_INV=User_Key.ETF_Investments[2], SEC4_INV=User_Key.ETF_Investments[3],
-                   MONEY_TRADED=User_Key.total_purchases(User_Key.ETF_Investments), ERROR_MESSAGE=error_msg, TIME1=time_purchased[0],
-                   TIME2=time_purchased[1], TIME3=time_purchased[2], TIME4=time_purchased[3],
-                   TIME5=((datetime.now() + timedelta(days=7)).strftime("%m/%d/%Y %H:00:00")))
-    else:
-        body = """ ERROR RECEIVED, DO SOMETHING HERE """
+
+    # Email Body (EDIT IF NEEDED)
+    body = """Starting Cash Balance = ${INIT_ACC_BAL}\nCash Balance Left = ${ACC_BAL}\n\nPurchases:\n{PURCHASES}\n\nNext Purchases date ~ {TIME}""".format(INIT_ACC_BAL=starting_balance, ACC_BAL=account_balance, PURCHASES=purchase_summary, TIME=((datetime.now() + timedelta(days=7)).strftime("%m/%d/%Y %H:00:00")))  # Default next purchase date in seven days. This can be changed. All depends on when you run script
 
     # Formatting Email
     mail = EmailMessage()
@@ -160,10 +256,16 @@ def email():
 
 # Function that logs in to Fidelity
 fidelity_login()
+
 # Function that loops to purchase the Securities
+
 num_of_purchases = fidelity_security_purchasing()
 num_of_purchases += 1
 
+# Functions that create the body of Email
+purchase_sum = email_purchases_creator(num_of_purchases)
+error_msg_sum = email_error_msg_creator()
 
+purchase_sum += error_msg_sum
 
-
+email(purchase_sum)
